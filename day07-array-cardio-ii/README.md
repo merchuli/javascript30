524c5eca91d9320682af733801a22e1d6f503efd
Merchuli <30795240+merchuli@users.noreply.github.com>
2022-10-22 20:45:41 +0800

    feat: Practice some array methods

---
+++ day07-array-cardio-ii/README.md
@@ -0,0 +1,6 @@
+- find
+  - The `find()` method returns **the first element** in the provided array that satisfies the provided testing function.
+  - If no values satisfy the testing function, **undefined** is returned.
+- findIndex
+  - The `findIndex()` method returns the index of **the first element** in an array that satisfies the provided testing function.
+  - If no elements satisfy the testing function, **-1** is returned.
--- day07-array-cardio-ii/index.js
+++ day07-array-cardio-ii/index.js
@@ -33,7 +33,22 @@ console.log('Array.prototype.every()', isAllAdults);
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const target = comments.find((comment) => comment.id === 823423);
+console.log('Array.prototype.find()', target);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+console.log('origin data');
+console.table(comments);
+const targetIndex = comments.findIndex((comment) => comment.id === 823423);
+console.log('findIndex', targetIndex);
+// comments.splice(targetIndex, 1);
+// console.log('delete row');
+// console.table(comments);
+
+const newComments = [
+  ...comments.slice(0, targetIndex),
+  ...comments.slice(targetIndex + 1),
+];
+console.table(newComments);
